de213dfb25e62c9f2fdcdf443ed29df04f4a47b6
fanidi <fanidi@fanidi.fanidi>
2024-03-14 12:19:25 -0400

    getenv + get_path_cmd

---
+++ environnement/utils.c
@@ -0,0 +1,44 @@
+#include"minishell.h"
+
+
+
+char *ft_getenv(t_env *env, char *key)
+{
+	t_env *tmp;
+	
+	tmp = env;
+	while (tmp && tmp->key)
+	{
+		if (ft_strncmp(tmp->key,key,ft_strlen(key)) == 0)
+			return (tmp->value);
+		tmp = tmp->next;
+	}
+	return (NULL);
+}
+
+char *get_path_cmd(t_env *env, t_cmd_node *cmd)
+{
+	char **path;
+	int i;
+	char *path_cmd;
+	char *util;
+
+	i = 0;
+	// /bin + / =  /bin/  + ls = /bin/ls
+	path = ft_split(ft_getenv(env, "PATH"), ":");
+	while (path[i] && cmd->executable)
+	{
+		util = ft_strjoin(path[i++], "/");
+		path_cmd = ft_strjoin(util, cmd->executable);
+		if (access(path_cmd, X_OK) != 1)
+		{
+			free(util);
+			ft_free(path);
+			return (path_cmd);
+		}
+		free(util);
+		free(path_cmd);
+		path_cmd = NULL;
+	}
+	return (path_cmd);
+}
