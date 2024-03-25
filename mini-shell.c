e1ecd6e7f37d23a79e653444d6a6c4c6ca8fe538
Abdellah Fanidi <afanidi@e2r4p12.1337.ma>
2024-03-25 23:06:06 +0000

    without color

--- mini-shell.c
+++ mini-shell.c
@@ -13,36 +13,37 @@ int main(int ac, char **av, char **env)
     env_copy(&env_list, env);
 
     while(1)
-    {
-        input = readline(YELLOW"➜ sh-mini ✗ "NC);
+    {   
+        input = readline("➜ sh-mini ✗ ");
         if (!input)
             exit(0);
         if (ft_strlen(input) > 0)
             add_history(input);
+            
         else 
             continue;
         //clear_history();
 
-        SplitTokens(input, &tokens);
-        tokenisation(&tokens,input);
-	    handling_qoutes(&tokens);
-        //print_tokens(tokens);
+    //     SplitTokens(input, &tokens);
+    //     tokenisation(&tokens,input);
+	//     handling_qoutes(&tokens);
+    //     //print_tokens(tokens);
 
        
-        //printf("\n\n\n");
-        t_tree_node *tree = parse_command(&tokens, env_list);
-        if (tree)
-        {
-            if (tree->type == REDIR || tree->type ==CMD)
-                execute_simple(tree, env_list);
-            else
-                execute(tree, env_list);
-        }
-        // printf("%d",execute_tree(tree, env_list));
+    //     //printf("\n\n\n");
+    //     t_tree_node *tree = parse_command(&tokens, env_list);
+    //     if (tree)
+    //     {
+    //         if (tree->type == REDIR || tree->type ==CMD)
+    //             execute_simple(tree, env_list);
+    //         else
+    //             execute(tree, env_list);
+    //     }
+    //     // printf("%d",execute_tree(tree, env_list));
         
-       if (ft_strncmp(input, "exit", 5) == 0)
-            break;
-        free_tokens(&tokens);
+    //    if (ft_strncmp(input, "exit", 5) == 0)
+    //         break;
+    //     free_tokens(&tokens);
         free(input);
     }
 }
