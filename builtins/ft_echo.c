21b2e04396e6069744a43279c50802b4d2569bc6
Abdellah Fanidi <afanidi@e2r4p12.1337.ma>
2024-03-25 02:52:40 +0000

    echo done

--- builtins/ft_echo.c
+++ builtins/ft_echo.c
@@ -5,19 +5,28 @@ int ft_echo(t_cmd_node *cmd)
     int i;
     int len;
     int flag;
+    int position;
 
     len = count_len(cmd->arguments);
     i = 1;
     flag = 0;
+    position = 0;
     while (i < len)
     {
         if (!ft_strncmp(cmd->arguments[i], "-n", 2) && i == 1)
         {
+            position = 1;
             flag = 1;
             i++;
             continue;
         }
+        if (flag == 1 && !ft_strncmp(cmd->arguments[i], "-n", 2) && position == 1)
+        {
+            i++;
+            continue;
+        }
         ft_putstr_fd(cmd->arguments[i], 1);
+        position = -1;
         if (i + 1 < len)
             ft_putstr_fd(" ", 1);
         i++;
